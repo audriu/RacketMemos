3e7d562c436cf41a80b28a6368092882c66287af
Audrius <vienas00@gmail.com>
2016-12-18 21:16:29 +0000

    nothing.

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 I keep my old racket sketches here.
+Also I sketch some game in gere.
--- initial.rkt
+++ initial.rkt
@@ -2,6 +2,6 @@
 (require "consts.rkt")
 (provide syntax (all-defined-out))
 
-(define initial-entities (cons(entity 0 20 7)(cons (entity 0 50 5)null)))
+(define initial-entities (cons(entity 0 20 7)(cons (entity 0 50 5)(cons (entity 0 190 2)null))))
 
 (define initial-terain null)
